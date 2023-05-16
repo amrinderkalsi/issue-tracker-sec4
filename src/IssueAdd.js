21a6d657a0866c9fbc46373bb22ebb2251c0a775
Ammy Kalsi <amrinder.kalsi13.com>
2023-05-15 20:42:33 -0400

    Added Class & Functional Components and Created Basic App Structure

---
+++ src/IssueAdd.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class IssueAdd extends Component {
+   
+    render() { 
+        return (
+            <div>
+                This is a placeholder for IssueAdd
+            </div>
+          );
+    }
+}
+ 
+export default IssueAdd;
---
+++ src/IssueFilter.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class IssueFilter extends Component {
+   
+    render() { 
+        return (
+            <div>
+                This is a placeholder for Issue Filter
+            </div>
+          );
+    }
+}
+ 
+export default IssueFilter;
